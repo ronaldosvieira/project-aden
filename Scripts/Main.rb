80eb435f3d5cfc32c2331642a18b915a86cd30e8
Leonardo <leonardostsouza@gmail.com>
2015-05-11 20:28:18 -0700

    Small modifications to testMap

--- Scripts/Main.rb
+++ Scripts/Main.rb
@@ -4,4 +4,11 @@
 #  This processing is executed after module and class definition is finished.
 #==============================================================================
 
+Font.default_name = "Old London" #for example
+Font.default_size = 22
+
+#Font.default_bold = true
+Font.default_color = Color.new(0,0,0)
+#Font.default_(etc)
+
 rgss_main { SceneManager.run }
